bd7086fc3037ee8b56dd15e160a275e7a6c291d9
pablo <pa.romerorgz@gmail.com>
2021-04-12 00:18:25 -0500

    add home things

--- monument-angular/src/app/pages/home/home.component.ts
+++ monument-angular/src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import SwiperCore, {Navigation, Pagination} from 'swiper/core';
+
+SwiperCore.use([Navigation, Pagination]);
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,38 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  slideOpts: {
+    initialSlide: 1;
+    speed: 400;
+  };
+  currentSlide: {
+    title: string,
+    description: string,
+    image: string
+  }
+  slides=[]
 
-  ngOnInit(): void {}
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.slides = [
+      {
+        title: 'Teacher of the Year',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+      mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+      voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+      fugiat iusto fuga praesentium optio, eaque rerum! Provident
+      similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+      eius earum ut molestias architecto voluptate aliquam nihil,
+      eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+      tenetur error, harum nesciunt ipsum debitis q`,
+      image: '../../../assets/monument-main-logo.png'
+      }
+    ];
+  this.currentSlide = this.slides[0]
+
+  }
+
 }
