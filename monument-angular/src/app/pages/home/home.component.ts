dd1fe3fb3021ce433e25bf518ab56ee19acc62d4
pablo <pa.romerorgz@gmail.com>
2021-04-13 23:11:05 -0500

    wip slider

--- monument-angular/src/app/pages/home/home.component.ts
+++ monument-angular/src/app/pages/home/home.component.ts
@@ -1,8 +1,14 @@
+import { slideOpts } from './slider.config';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import SwiperCore, {Navigation, Pagination} from 'swiper/core';
+import { loremIpsum } from 'lorem-ipsum';
 
-SwiperCore.use([Navigation, Pagination]);
-
+type Slide = {
+  isBeginningSlide: boolean;
+  isEndSlide: boolean;
+  title: string;
+  description: string;
+  image: string;
+};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,38 +16,72 @@ SwiperCore.use([Navigation, Pagination]);
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-  slideOpts: {
-    initialSlide: 1;
-    speed: 400;
-  };
-  currentSlide: {
-    title: string,
-    description: string,
-    image: string
-  }
-  slides=[]
+  currentSlide: Slide;
+  slides: Slide[];
 
-  constructor() {
+  slideOpts = slideOpts.coverflow;
+  lorem = loremIpsum({
+    units: 'paragraph',
+  });
 
-  }
+  constructor() {}
 
   ngOnInit(): void {
     this.slides = [
       {
+        isBeginningSlide: true,
+        isEndSlide: false,
         title: 'Teacher of the Year',
-        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-      mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-      voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-      fugiat iusto fuga praesentium optio, eaque rerum! Provident
-      similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-      eius earum ut molestias architecto voluptate aliquam nihil,
-      eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-      tenetur error, harum nesciunt ipsum debitis q`,
-      image: '../../../assets/monument-main-logo.png'
-      }
+        description: this.lorem,
+        image: '../../../assets/monument-main-logo.png',
+      },
+      {
+        isBeginningSlide: true,
+        isEndSlide: false,
+        title: 'Student of the Year',
+        description: this.lorem,
+        image: '../../../assets/images/hero-main.png',
+      },
     ];
-  this.currentSlide = this.slides[0]
 
+    this.currentSlide = this.slides[0];
   }
 
+  slideNext(slideView, i) {
+    console.log({ slideView });
+    slideView.slideNext(500).then((d) => {
+      console.log({ d });
+      this.checkIfNavDisabled(slideView, i);
+    });
+  }
+
+  //Move to previous slide
+  slidePrev(slideView, i) {
+    slideView.slidePrev(500).then(() => {
+      this.checkIfNavDisabled(slideView, i);
+    });
+  }
+
+  //Method called when slide is changed by drag or navigation
+  SlideDidChange(slideView, i) {
+    this.checkIfNavDisabled(slideView, i);
+  }
+
+  //Call methods to check if slide is first or last to enable disbale navigation
+  checkIfNavDisabled(slideView, i) {
+    console.log('is disabled', { slideView });
+    this.checkisBeginning(slideView, i);
+    this.checkisEnd(slideView, i);
+  }
+
+  checkisBeginning(slideView, i) {
+    slideView.isBeginning().then((istrue) => {
+      this.slides[i].isBeginningSlide = istrue;
+    });
+  }
+  checkisEnd(slideView, i) {
+    slideView.isEnd().then((istrue) => {
+      this.slides[i].isEndSlide = istrue;
+    });
+  }
 }
