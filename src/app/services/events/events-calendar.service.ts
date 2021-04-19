61df5789139e4d9b5fd1947a735060151c4bfd82
Ivan Romero <ivanrr308@gmail.com>
2021-04-19 14:06:35 -0500

    Update events-calendar.service.ts

--- src/app/services/events/events-calendar.service.ts
+++ src/app/services/events/events-calendar.service.ts
@@ -2,35 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Content {
+  type: string, // title, info, paragraph, closing, action
+  content: string
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsCalendarService {
    private baseUrl = 'http://localhost:8000/news-events';
-    content = {
-    title: 'Interested in joining our team?',
-    info: '<b> Attend </b> our upcoming virtual job fair! Register below or email careers@monumentacademydc.org with any questions.',
-    paragraph: 'Tuesday, March 16th, 4 PM – 6 PM Our next open board meeting is Wednesday, March 17th at 6 PM. Register here.',
-    closing: 'Check out our recent feature on CBS This Morning HERE!'
-  }
-
-
   constructor(private http: HttpClient) { }
 
- getAll(): Observable<any[]> {
+ getAll(): Observable<Content[]> {
     return this.http.get<any>(this.baseUrl);
   }
-get(id: string): Observable<any> {
-    return this.http.get<any>(this.baseUrl + '/' + id);
-  }
-create(country: any) {
-    return this.http.post<any>(this.baseUrl, country);
-  }
-update(id: string, country: any): Observable<any> {
-    return this.http.put<any>(this.baseUrl + '/' + id, country);
-  }
-delete(id: string) {
-    return this.http.delete<any>(this.baseUrl + '/' + id);
-  }
+// get(id: string): Observable<Content> {
+//     return this.http.get<any>(this.baseUrl + '/' + id);
+//   }
+// create(country: Content) {
+//     return this.http.post<any>(this.baseUrl, country);
+//   }
+// update(id: string, country: Content): Observable<Content> {
+//     return this.http.put<any>(this.baseUrl + '/' + id, country);
+//   }
+// delete(id: string) {
+//     return this.http.delete<any>(this.baseUrl + '/' + id);
+//   }
 }
