9ffb04f1840371d7e8445185788ac02175400963
pablo <pa.romerorgz@gmail.com>
2021-06-10 12:21:52 -0500

    add new cal

--- src/app/pages/updates-calendar/updates-calendar.component.ts
+++ src/app/pages/updates-calendar/updates-calendar.component.ts
@@ -11,14 +11,14 @@ export class UpdatesCalendarComponent implements OnInit {
 
   calendars = [
     {
-    year: '2020-2021',
-    pdf: '../../../assets/documents/2020-2021 Calendar  - 2020-21 Calendar.pdf'
+      year: '2020-2021',
+      pdf: '../../../assets/documents/2020-2021 Calendar  - 2020-21 Calendar.pdf',
     },
     {
-    year: '2021-2022',
-    pdf: '../../../assets/documents/2021-2022-Calendar.pdf'
-    }
-  ]
+      year: '2021-2022',
+      pdf: '../../../assets/documents/2021-2022 Monument Academy Calendar_External-Facing - 2021-22 Calendar.pdf',
+    },
+  ];
 
   constructor() {}
 
