df351de5f48c978e7b8dfb02fc5958d211eaf58c
Ivan Romero <38643671+CIRR14@users.noreply.github.com>
2021-09-09 14:13:42 -0500

    Update family-resources.component.ts

--- src/app/pages/family-resources/family-resources.component.ts
+++ src/app/pages/family-resources/family-resources.component.ts
@@ -2,7 +2,7 @@ import { environment } from './../../../environments/environment';
 import { isEmpty } from 'lodash';
 import { ApiService } from 'src/app/services/api/api.service';
 import { BehaviorSubject } from 'rxjs';
-import { Component, Injectable, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-family-resources',
@@ -10,7 +10,7 @@ import { Component, Injectable, OnInit } from '@angular/core';
   styleUrls: ['./family-resources.component.scss'],
 })
 @Injectable({ providedIn: 'root' })
-export class FamilyResourcesComponent implements OnInit {
+export class FamilyResourcesComponent implements OnInit, OnDestroy {
   images = '../../../assets/images/';
   items = [
     'Home visits',
@@ -21,7 +21,7 @@ export class FamilyResourcesComponent implements OnInit {
     'Volunteering',
   ];
 
-  uploadUrl = 'http://localhost:1337';
+  uploadUrl = environment.apiUrl;
 
   resourcesHC = [
     {
@@ -105,11 +105,13 @@ export class FamilyResourcesComponent implements OnInit {
 
   resources: any;
   resourcesSub = new BehaviorSubject([]);
+  $getFamResources;
 
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
-    this.resources = this.api.getData('familyResources').subscribe(result => {
+    this.$getFamResources = this.api.getData('familyResources');
+    this.resources = this.$getFamResources.subscribe(result => {
       console.log(result);
       if (!isEmpty(result)) {
         this.resources = result?.data.sort((a: any, b: any)  => a.id.value - b.id.value);
@@ -117,5 +119,9 @@ export class FamilyResourcesComponent implements OnInit {
     });
 
     this.uploadUrl = environment.apiUrl;
+  }
+
+  ngOnDestroy(): void {
+    this.$getFamResources.unsubscribe();
   }
 }
