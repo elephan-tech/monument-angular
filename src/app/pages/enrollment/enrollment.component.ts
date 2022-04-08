33c6813c0cdac2c49dc9955c294f4f3e1619f2c8
Ivan Romero <ivanrr308@gmail.com>
2022-04-07 20:00:07 -0500

    Apr. 7, 2022 updates on enrollment page

--- src/app/pages/enrollment/enrollment.component.ts
+++ src/app/pages/enrollment/enrollment.component.ts
@@ -9,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class EnrollmentComponent implements OnInit {
   pageTitle: string;
   heroImg: string;
+  MSDCSeatAcceptanceForm: string;
+  DCResidencyForm: string;
   constructor() {}
 
   ngOnInit(): void {
+    this.MSDCSeatAcceptanceForm = '../../../assets/documents/MSDC-Seat-Acceptance-Form-22-23.pdf';
+    this.DCResidencyForm = '../../../assets/documents/DC-Residency-Verification-Form-22-23.pdf';
     this.pageTitle = 'Enrollment';
     this.heroImg = 'assets/images/monument-6.png';
   }
