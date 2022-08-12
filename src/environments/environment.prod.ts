a397fe76ee27bacdd811f7deccc9c7ec9614633f
talha195javed <talha195javed@gmail.com>
2022-08-12 15:59:26 +0500

    Instafeed test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://admin.monumentacademy.org',
+  access_token: 'IGQVJWSEJWVHY5U05Ialk0WHdMS0dRX25KN2E3Wkt2VVE5OWlyZA1QyQUxzOTl4eGpZAaEFuaHFaRy1WVGhSaXB4SXJLSk9ZALW44c3REVWxNckhLQVJkT1F1dkhmVi1FM0RsSnExZAGZAPVGFZAWm9LaTJ0UwZDZD',
 };
