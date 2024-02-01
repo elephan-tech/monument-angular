26870b682b80f6a809cf04328c5b7cf063efcb9f
talha195javed <talha195javed@gmail.com>
2024-02-01 17:55:52 +0500

    previous images

--- src/app/pages/programs/academy-celebrate/academy-celebrate.component.ts
+++ src/app/pages/programs/academy-celebrate/academy-celebrate.component.ts
@@ -21,6 +21,19 @@ export class AcademyCelebrateComponent {
     {image: 'assets/images/celebration/Room Pic.jpeg', title: 'Teacher of the Year'},
   ];
 
+  previouses = [
+    {image: 'assets/images/archived/Band.jpeg', title: 'Band'},
+    {image: 'assets/images/archived/Dancing.jpeg', title: 'Dancing'},
+    {image: 'assets/images/archived/Dr-Grant-and-Dr-DeCruise.jpeg', title: 'Dr. Grant and Dr. DeCruise'},
+    {image: 'assets/images/archived/Homepage.jpeg', title: 'Team'},
+    {image: 'assets/images/archived/LeadershipTeam.jpeg', title: 'Leadership Team'},
+    {image: 'assets/images/archived/MA22.jpeg', title: 'MA 22'},
+    {image: 'assets/images/archived/Programs.jpeg', title: 'Programs'},
+    {image: 'assets/images/archived/Staff-Member-of-the-Year.jpeg', title: 'Staff Member of the Year'},
+    {image: 'assets/images/archived/Table.jpeg', title: 'Table'},
+    {image: 'assets/images/archived/Teacher-of-the-Year.jpeg', title: 'Teacher of the Year'},
+  ];
+
   showLightbox(i: any) {
     this.currentIndex = i;
     this.showFlag = true;
@@ -29,5 +42,11 @@ export class AcademyCelebrateComponent {
   closeEventHandler() {
     this.showFlag = false;
     this.currentIndex = -1;
+  }
+
+  showPreviousContent: boolean = false;
+
+  togglePreviousContent() {
+    this.showPreviousContent = !this.showPreviousContent;
   }
 }
